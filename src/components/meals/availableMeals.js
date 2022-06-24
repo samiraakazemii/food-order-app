c2e554f5d4c58129e675658d5b610c63c50103eb
samiraakazemii <samirakazemi0203@gmail.com>
2022-06-24 23:27:12 +0430

    GetMealsHttp

--- src/components/meals/availableMeals.js
+++ src/components/meals/availableMeals.js
@@ -1,44 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../ui/card/card";
 import MealsItem from "./mealsItem/mealsItem";
+import UseHttp from "../../hooks/use-http";
+import loadingGif from "../../assets/Loading.gif";
 import style from "./availableMeals.module.css";
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
-  const MealsList = DUMMY_MEALS.map((meal) => (
+  const { isLoading, error, RequestHttp: FetchMeals } = UseHttp();
+  const [Meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const TransformData = (data) => {
+      let dataArray = [];
+      for (const key in data) {
+        dataArray.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(dataArray);
+    };
+
+    FetchMeals(
+      {
+        url: "https://food-order-46fef-default-rtdb.firebaseio.com/meals.json",
+      },
+      TransformData
+    );
+  }, [FetchMeals]);
+
+  const MealsList = Meals.map((meal) => (
     <MealsItem key={meal.id} meals={meal} />
   ));
+
+  // if (isLoading) {
+  //   return (
+  //     <>
+  //       <p className={style.Loading}>Loading...</p>
+  //       <img
+  //         className={style["loading-mask"]}
+  //         src={loadingGif}
+  //         alt="wait until the page loads"
+  //       />
+  //     </>
+  //   );
+  // }
+  // if (error) {
+  //   return (
+  //     <>
+  //       <p className={style.ErrorHttp}>{error}</p>
+  //     </>
+  //   );
+  // }
+
   return (
-    <section className={style.meals}>
-      <Card className="mealCard">
-        <ul>{MealsList}</ul>
-      </Card>
-    </section>
+    <>
+      {!isLoading && Meals.length > 0 && !error && (
+        <section className={style.meals}>
+          <Card className="mealCard">
+            <ul>{MealsList}</ul>
+          </Card>
+        </section>
+      )}
+      {!isLoading && error && (
+        <>
+          <p className={style.ErrorHttp}>{error}</p>
+        </>
+      )}
+      {isLoading && (
+        <>
+          <p className={style.Loading}>Loading...</p>
+          <img
+            className={style["loading-mask"]}
+            src={loadingGif}
+            alt="wait until the page loads"
+          />
+        </>
+      )}
+    </>
   );
 };
 
---
+++ src/hooks/use-http.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useState, useCallback } from "react";
+
+const UseHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const RequestHttp = useCallback(async (reqconfig, applyData) => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const { data } = await axios(reqconfig.url, {
+        method: reqconfig.method ? "GET" : "",
+        headers: reqconfig.headers ? reqconfig.headers : {},
+        body: reqconfig.body ? reqconfig.body : null,
+      });
+      applyData(data);
+    } catch (err) {
+      if (err.response) {
+        // The client was given an error response (5xx, 4xx)
+        console.log(err.response);
+      } else if (err.request) {
+        // The client never received a response, and the request was never left
+        console.log(err.request);
+      } else {
+        // Anything else
+        console.log(err);
+      }
+      setError(err.message);
+    }
+    setIsLoading(false);
+  }, []);
+  return { isLoading, error, RequestHttp };
+};
+
+export default UseHttp;
